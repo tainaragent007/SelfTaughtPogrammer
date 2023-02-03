484e2e31ce315de91f39cf4aa32f679b1b40be57
tainaragent <tainaragent007@gmail.com>
2023-02-03 01:14:35 -0300

    more stuff, need to check on the exceptions

---
+++ Ex_py/7_Fuctions_tests.py
@@ -0,0 +1,12 @@
+
+# Functions Exercise 1
+# Write a function named square 
+# that takes a number, n, as a 
+# parameter and returns that 
+# number squared.
+
+def square(n):
+    squared=n*n
+    return squared
+
+print('squared')
---
+++ Ex_py/8_Builtinfunct.py
@@ -0,0 +1,13 @@
+
+# Built-In Functions Exercise
+# Write a function called convert 
+# that takes a string as a parameter, 
+# converts it to a float and returns 
+# the result. Write a docstring that 
+# lets the user of the function know 
+# the string needs to be a number 
+# like this "100".
+
+def convert(a_string):
+    
+    return float(a_string)
